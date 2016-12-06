15cb5ebdf3cdc51598a66d73dd99d7fe8f3588f9
pmape <pmape@WANESSAMACIEL>
2016-12-05 23:31:03 -0200

    Criando repositório.

---
+++ ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string email { get; set; }
+        public DateTime DataCadstro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadstro.Year >= 5;
+        }
+
+    }
+}
